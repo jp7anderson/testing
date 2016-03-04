b7f09b438b14092350c2b684a58497c83ea7c5d1
Anderson Dario <anderson@jp7.com.br>
2016-03-04 15:06:48 -0300

    Video aula 11 - Expressoes regulares

---
+++ tests/ExpressionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class ExpressionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     *
+     */
+    function it_finds_a_string()
+    {
+        $regex = Expression::make()->find('www');
+        $this->assertTrue($regex->test('www'));
+
+        $regex = Expression::make()->then('www');
+        $this->assertTrue($regex->test('www'));
+    }
+
+    /**
+     * @test
+     *
+     */
+    function it_checks_for_anything()
+    {
+        $regex = Expression::make()->anything();
+
+        $this->assertTrue($regex->test('foo'));
+    }
+
+    /**
+     * @test
+     *
+     */
+    function it_maybe_has_a_value()
+    {
+        $regex = Expression::make()->maybe('http');
+
+        $this->assertTrue($regex->test('http'));
+        $this->assertTrue($regex->test(''));
+    }
+
+    /**
+     * @test
+     *
+     */
+    function it_can_chain_method_calls()
+    {
+        $regex = Expression::make()
+            ->find('foo')
+            ->maybe('bar')
+            ->then('biz');
+
+        $this->assertTrue($regex->test('foobarbiz'));
+    }
+
+    /**
+     * @test
+     *
+     */
+    function it_exclude_values()
+    {
+        $regex = Expression::make()
+            ->find('foo')
+            ->anythingBut('bar')
+            ->then('biz');
+
+        $this->assertTrue($regex->test('foobazbiz'));
+        $this->assertFalse($regex->test('foobarbiz'));
+    }
+}
